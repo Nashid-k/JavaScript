3acea956185b3b56f5e3bc0a21d61cf92b89410b
Nashid.k <151704391+Nashid-k@users.noreply.github.com>
2024-10-31 18:49:28 +0000

    conversionOperation of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "hello"
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+
+// console.log(valueInNumber);
+
+
+
+//"33" => 33
+//"33abc" = NaN still gives typeof number
+//true => 1; false =>0;
+
+
+let isLoggedIn = "dsfg"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+//"any values" => true
+
+
+
+
+
+let someNumber = 33
+
+// let stringNumber = someNumber.toString() or  let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
