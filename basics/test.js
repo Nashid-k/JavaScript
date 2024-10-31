735cccf253915fa364d7c3c3cbee8783575c5c40
Nashid.k <151704391+Nashid-k@users.noreply.github.com>
2024-10-31 17:58:11 +0000

    variables

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,13 @@
-console.log('hi');
+const accoundId = 1234567890;
+let accountEmail = "abd@google.com";
+var accountPass = "121211";
+accountCity = "jaipur";
+let accountState;
+console.table([accoundId, accountEmail, accountPass, accountCity, accountState])
+
+
+/*
+prefer not to use var
+because of block scope and functional scope
+
+*/
